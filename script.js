3e5329b1735e4c50301621c57e377217e61f623f
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-27 16:20:55 +0300

    question 1 completed

---
+++ script.js
@@ -0,0 +1,23 @@
+//! Adımlar:
+//? header classına sahip olan elementi querySelector() metodu ile yakalayınız. Background color, color, padding özellikleri ekleyiniz ve text hizalamasını center olarak ayarlayınız.
+
+const header = document.querySelector(".header");
+header.style.backgroundColor = "orange";
+header.style.color = "white";
+header.style.padding = "1.5rem";
+header.style.textAlign = "center";
+
+
+//? title id sine sahip olan elementi getElementById() metodunu kullanarak yakalayınız. Ve "Javascript Dom Assignment 1" metnini textContent özelliği ile ekleyiniz.
+
+
+
+//? nav-item classına sahip olan elementi querySelector() metodu ile yakalayınız. Flexbox(center olsun) özelliği ekleyiniz ve elementler arasına gap özelliği ile boşluk ekleyiniz. List style özelliğini kaldırınız.
+
+//? username ve password idsine sahip olan inputları querySelector() metodu ile yakalayınız. İnputların value attributelarını kullanarak Username inputuna "Anthony" yazınız, password inputuna "123456" yazınız. Ayrıca password inputuna yazılan değerin görülmesini sağlayın. Ve inputların disabled özelliğini aktif ediniz. (Not: html de input type=password olduğunda bildiğiniz üzere yazılan değerler saklanıyor. Bu özelliği js ile değiştirmeniz lazım.)
+
+//? btn classına sahip olan elementi querySelector() metodu ile yakalayınız. Background color, color, padding, border-radius özellikleri ekleyiniz. Cursor pointer olsun ve border özelliği none olsun. Ayrıca button elementinin metni "Giriş Yap" metni olsun.
+
+//? projects idsine sahip olan elementi getElementById() metodunu kullanarak yakalayınız. Yakaladığınız bu element üzerinden bu elementin first childi olan h3 elementine ulaşıp metnini "Js Dom Projects" olarak değiştiriniz.
+
+//? const myProjects = ["Hello World!","Guess Number","Checkout Page","Gelir-Gider Projesi","Api Projects"]; Verilen myProjects arrayindeki elemanları bir önceki adımda yakaladığınız projects idsine sahip olan elementin last childi olan ul elementine innerHTML özelliğini kullanarak tek tek li elementleri içerisinde child olarak ekleyiniz.
